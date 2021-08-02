9f3048cbbd19e3802ff3635e1c533d753e298d11
Md Adil Khan <mdadilakhtar8@gmail.com>
2021-08-02 16:43:10 +0530

    completed signup and login

---
+++ src/index.server.js
@@ -0,0 +1,42 @@
+//remote name-origin
+//branch master
+
+const express = require("express");
+const env=require("dotenv");
+const app=express();
+const mongoose=require("mongoose");
+
+
+//routes
+const authRoutes=require("./routes/auth");
+
+
+ // environment variable or consttant
+ env.config();
+
+//  mongodb connection
+//  mongodb+srv://root:<password>@cluster0.ccz2p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+ mongoose.connect(
+        `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.ccz2p.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+     {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      useCreateIndex:true
+     })
+     .then(()=>console.log("databse connected..."))
+     .catch(err=>console.log(err));
+
+
+      
+   
+
+
+
+
+app.use(express.json());//using middlewere for passing data
+
+app.use('/api',authRoutes);
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is running on port ${process.env.PORT}`);
+})
